99e88e356a1acb975a9266851d12c1dda73061a9
Mélodie <melodie.kouy@gmail.com>
2023-02-24 19:33:38 +0100

    TP : Random

--- TP2/sketch.js
+++ TP2/sketch.js
@@ -1,43 +1,45 @@
 
-// let color = [,,,,,];
-let carre_taille = 50;
+let COLOR = [
+  [97, 8, 84],      // Ash gray
+  [244, 28, 21],    // Raisin black
+  [335, 16, 41],    // Wenge
+  [353, 32, 96],    // Cherry blossom pink
+  [335, 19, 97],    // Fairy Tale
+  [190, 78, 55]     // Teal
+];
+
+let SQUARE_SIZE = 50;
+let CANVA_SIZE = 800;
 
 function setup() {
-  let size = 800;
-  createCanvas(size, size); 
-  // frameRate(2); 
+  createCanvas(CANVA_SIZE, CANVA_SIZE); 
+  frameRate(2); 
   colorMode('HSB', 100); // = Hue / Teinte, Saturation, Brightness/Brillance
 }
 
 function draw() {
-  // par defaut 60 frames / secondes
   background(0);
-  
-  grille();
-  // carre(100, 100);
+  grille(12, 12);
 }
 
-// Note : les coord (0,0) haut en gauche
+function grille(line, column){ // size de la grille == 600;
+  let border_width = (CANVA_SIZE - line * SQUARE_SIZE) / 2;
+  let border_height = (CANVA_SIZE - column * SQUARE_SIZE) / 2;
 
-function grille(){
-  let size = 600;
-  let border = carre_taille;
-
-  stroke(255);
-  for (let i = 1; i <= 12; i++) {
-    carre(carre_taille * i, border);
-    // for(let j = 1, j < 12; j++){
-    //   carre(carre_taille * i, border * 2);
-    // }
-    line(border, carre_taille * i, size + border, carre_taille * i);
-    line(carre_taille * i, border, carre_taille * i, size + border);
+  for (let i = 0; i < line; i++) {   
+    for (let j = 0; j < column; j++) {
+      drawSquare(border_width + SQUARE_SIZE * i, border_height + SQUARE_SIZE * j);
+    }
   }
-
 }
 
-function carre(x, y){
-  let c = [random(255), random(50), random(50)];
-  fill(c);
+function drawSquare(x, y){
+  let pick = [random(255), random(50), random(50)];
+  // let pick = [50, 50, 50];
+  // let pick = random(COLOR.length);
+  console.log(pick + " " +  COLOR[pick]);
+  // let pick = COLOR[random(COLOR.length - 1)];
+  fill(pick);
   stroke(100);
-  square(x, y, carre_taille);
+  square(x, y, SQUARE_SIZE);
 }
