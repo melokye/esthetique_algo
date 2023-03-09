3a9a9e1a9bb4dcf75c60cd1f68f0024e1f089a0a
Mélodie KOUY <melodiekouy@gmail.com>
2023-03-09 18:13:13 +0100

    update template

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,13 +1,10 @@
 function setup() {
   createCanvas(400,400); 
-  // frameRate(2); 
+  // frameRate(2); // par defaut 60 frames / secondes
 }
 
 function draw() {
-  // par defaut 60 frames / secondes
-
-  // background(random(128)); // entre 0 (noir) et 255 (blanc)
-  background(128);
+  background(random(128)); // entre 0 (noir) et 255 (blanc)
   
   ellipse(55,55,55,55);
   fill(255, 0, 0);
@@ -15,7 +12,6 @@ function draw() {
 
   stroke(255, 204, 100);
   line(20,20,200,200);
-
 }
 
 // Note : les coord (0,0) haut en gauche
