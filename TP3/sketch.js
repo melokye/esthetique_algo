1155b1213e6eb78bf53b15978d91be6fa25aad62
Mélodie KOUY <melodiekouy@gmail.com>
2023-03-09 21:57:51 +0100

    test curve

--- TP3/sketch.js
+++ TP3/sketch.js
@@ -4,53 +4,26 @@ function setup() {
   frameRate(1);
 }
 
-function draw() {
-  background(0);
+function draw(){
   const n = 10; // TODO 100
-  let x = width / 4;
-  let y = height / 5;
+  const x = width / 4, y = height / 5;
   const pas = y * 3 / n;
   
+  background(0);
   stroke(255); // couleur des lignes
-  noFill();
-  for (let i = 0; i < n; i++) { 
-    beginShape();
-    /* 
-      Note : 
-      - il faut au minimum 4 appel de la fonction 
-      curveVertex pour que ça affiche quelque chose
-      - il faut commencer de la gauche vers la droite
-    */
-    stroke(255); // couleur des lignes
-    curveVertex(x * 3, y + i * pas); // fin
-    curveVertex(x * 3, y + i * pas); // forcer la fin
-
+  noFill(); // TODO peut être ineressant de mettre fill en noir
+  for (let i = 0; i < n; i++) {
     pic(x, x * 3, y + i * pas, pas / 2);
-
-    curveVertex(x, y + i * pas); // pour forcer le point de début
-    curveVertex(x, y + i * pas); // début
-    // signal(x + x / 3, x * 2, y + i * pas , pas);
-    // signal(x * 2, x * 3 - 2*x/3, y + i * pas , pas);    
-
-    endShape();
   }
 
   stroke(250, 0, 0); // couleur des lignes
   line(x, y, x * 3, y); // TODO à supp, test pour le pas
 }
 
-function pic(x, x_max, y, max_height) {
-  /*
-  let progress = 0;
-  while(progress < x * 3){
-    progress += random(x_max - x - progress);
-  }
-  */
-  curveVertex(x_max, y);
-  curveVertex(x_max - random(x_max - x), y - random(max_height));
-  curveVertex(x, y);
-
-  // curveVertex(x + x/3, y - noiseVal);
-  // curveVertex(x_max - 2*x/3,  y  - noiseVal);
-
+function pic(x, x_max, y, y_max){
+  let progress = random(x_max - x);
+  curve(x, y, x, y, 
+    x_max - progress, y - random(y_max),
+    x_max, y);
+  return progress;  
 }
