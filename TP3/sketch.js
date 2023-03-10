e169cd7564a57270ad7119cf925751af81855ea0
Mélodie <melodiekouy@gmail.com>
2023-03-10 18:12:28 +0100

    test avec line

--- TP3/sketch.js
+++ TP3/sketch.js
@@ -5,25 +5,30 @@ function setup() {
 }
 
 function draw(){
-  const n = 10; // TODO 100
+  const n = 10; // TODO 80
   const x = width / 4, y = height / 5;
   const pas = y * 3 / n;
-  
+
+  let noiseVal =  noise(pas) * pas;
   background(0);
   stroke(255); // couleur des lignes
   noFill(); // TODO peut être ineressant de mettre fill en noir
+
   for (let i = 0; i < n; i++) {
-    pic(x, x * 3, y + i * pas, pas / 2);
-  }
+    let progress = x;
+    line(x, y + i * pas, x + 2*x / 3, y + i * pas);
+    // progress += pic(progress, x + x / 3, y + i * pas, pas / 2);
+    // progress += pic(progress, x + x / 3, y + i * pas, pas);
+    // progress += pic(progress, x + x / 3, y + i * pas, pas / 2);
+    line(x , y + i * pas, width - x, y + i * pas);
+  }  
 
   stroke(250, 0, 0); // couleur des lignes
   line(x, y, x * 3, y); // TODO à supp, test pour le pas
 }
 
 function pic(x, x_max, y, y_max){
-  let progress = random(x_max - x);
-  curve(x, y, x, y, 
-    x_max - progress, y - random(y_max),
-    x_max, y);
+  let progress = noise(y);
+  line(x, y, x, y - progress * y_max);
   return progress;  
 }
