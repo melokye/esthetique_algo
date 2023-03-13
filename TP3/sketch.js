feddb7d61101e42faa365670fe8dddb6f08413ea
Mélodie <melodiekouy@gmail.com>
2023-03-13 11:27:39 +0100

    sauvegarde

--- TP3/sketch.js
+++ TP3/sketch.js
@@ -1,34 +1,55 @@
 function setup() {
-  const CANVA_SIZE = 700; // TODO à modifier dans les autres TP
+  const CANVA_SIZE = 700;
   createCanvas(CANVA_SIZE, CANVA_SIZE);
-  frameRate(1);
+  noLoop(); // On a besoin de l'exécuter qu'une fois
 }
 
 function draw(){
-  const n = 10; // TODO 80
+  const n = 20; // TODO 80
+  
+  // Coordonnées de "point"
   const x = width / 4, y = height / 5;
+  
   const pas = y * 3 / n;
-
-  let noiseVal =  noise(pas) * pas;
+  
   background(0);
   stroke(255); // couleur des lignes
-  noFill(); // TODO peut être ineressant de mettre fill en noir
+  
+  let seed = 0.00;
+  for (let i = 0; i < n; i++){
 
-  for (let i = 0; i < n; i++) {
-    let progress = x;
-    line(x, y + i * pas, x + 2*x / 3, y + i * pas);
-    // progress += pic(progress, x + x / 3, y + i * pas, pas / 2);
-    // progress += pic(progress, x + x / 3, y + i * pas, pas);
-    // progress += pic(progress, x + x / 3, y + i * pas, pas / 2);
-    line(width - x - 2*x / 3 , y + i * pas, width - x, y + i * pas);
-  }  
-
-  stroke(250, 0, 0); // couleur des lignes
-  line(x, y, x * 3, y); // TODO à supp, test pour le pas
+    line(x, y + i * pas, 
+         x + 2*x / 3, y + i * pas); // début
+    
+    let j = x + 2/3 * x;
+    let wave = y + i * pas; // TODO renommer wave ?
+    
+    while(j < width - x - 2*x / 3){ // TODO modif la condition
+      // TODO modif seed / max pour que ça marche aussi avec beaucoup de n (jsp trop cmt fctne noise en fait)
+      let tmp_x = coord(j, n + seed, j); 
+      let tmp_y = coord(y + i * pas, - (pas + seed), j); 
+      
+      seed += 0.01;
+      line(j, wave, tmp_x, tmp_y);
+      j = tmp_x;
+      wave = tmp_y;
+    }
+    
+    // pour relier la 2e et la 3e partie
+    line(j, wave, 
+         width - x - 2*x / 3, y + i * pas); 
+    
+    line(width - x - 2*x / 3 , y + i * pas, 
+         width - x, y + i * pas); // fin
+  }
+  
+  /*
+  // TODO à supp, test / ref
+  stroke(250, 0, 0);
+  line(x, y, x * 3, y);
+  */
 }
 
-function pic(x, x_max, y, y_max){
-  let progress = noise(y);
-  line(x, y, x, y - progress * y_max);
-  return progress;  
+function coord(min, max, seed){
+  return min + noise(seed) * max;
 }
